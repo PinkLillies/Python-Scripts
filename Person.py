463885b41fed16f62e11d9f39d51d398d51069ee
PinkLillies <53938392+PinkLillies@users.noreply.github.com>
2021-11-22 20:09:20 +1100

    Delete Person.py

--- Person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    '''A base class to defin Person Properties'''
-    count = 0
-    def __init__(self,name):
-        self.name = name
-        Person.count +=1
-    def speak(self):
-        return self.name     
