37bbda461901798d7f6e0d901c267bfefcf2c8e6
PinkLillies <53938392+PinkLillies@users.noreply.github.com>
2021-11-22 20:12:54 +1100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-Print("hello")
+# this will just print hello
+print("hello")
+
+# Let's create a variable called name and a variable called age
+
+name = "Bianca"
+age = 42
+print("Name:", name)
+print("Age:", age)
