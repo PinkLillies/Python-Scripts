60cd1f75829caf61239d17eb30d5eeba04e57a41
PinkLillies <53938392+PinkLillies@users.noreply.github.com>
2021-11-22 20:09:13 +1100

    Delete Bird.py

--- Bird.py
+++
@@ -1,8 +0,0 @@
-class Bird:
-    '''A base class to define bird properties.'''
-    count = 0
-    def __init__(self, chat):
-        self.sound = chat
-        Bird.count +=1
-    def talk(self):
-        return self.sound
